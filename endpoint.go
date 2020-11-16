745dae7d27375fbadfe0ba07f2a1aa85890c9bb9
Ignacio Carvajal Duran <ignaciocarvajald@gmail.com>
2020-11-15 23:41:13 -0300

    add TodoEndPoint struct to file

--- endpoint.go
+++ endpoint.go
@@ -1,1 +1,12 @@
 package todo
+
+import "github.com/go-kit/kit/endpoint"
+
+// Endpoints collects all endpoints which compose the Todo service
+type TodoEndpoints struct {
+	GetAllForUserEndPoint endpoint.Endpoint
+	GetByIDEndpoint       endpoint.Endpoint
+	AddEndpoint           endpoint.Endpoint
+	UpdateEndpoint        endpoint.Endpoint
+	DeleteEndpoint        endpoint.Endpoint
+}
